13ca5a5ee7c013d147d21d87259d2ea85553cf12
shota-tsuji <shotatuzi@gmail.com>
2023-09-16 13:02:29 +0900

    fix: Type errors are fixed

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 struct BWT {
     // Length of original text
-    size: usize,
+    size: u64,
     // Index of first character of the original text
     head: usize,
     // Original text
     str: Option<String>,
     // Suffix Array
-    sa: Vec<usize>,
+    sa: Vec<u64>,
 }
 
 impl BWT {
@@ -19,12 +19,12 @@ impl BWT {
         }
     }
 
-    fn sa2char(&self, i: usize, depth: usize) -> u8 {
-        let offset = (self.sa[i] + depth) % self.size;
+    fn sa2char(&self, i: usize, depth: u64) -> u8 {
+        let offset = ((self.sa[i] + depth) % self.size) as usize;
         self.str.as_ref().unwrap().chars().nth(offset).unwrap().to_ascii_lowercase() as u8
     }
 
-    fn sort(&mut self, begin: usize, end: usize, depth: usize) {
+    fn sort(&mut self, begin: i64, end: i64, depth: u64) {
         let mut a = begin;
         let mut b = begin;
         let mut c = end;
@@ -35,47 +35,47 @@ impl BWT {
         }
 
         //let pivot_pos = rand::random::<usize>() * size;
-        let pivot_pos = (rand::random::<f64>() * size as f64) as usize;
-        let pivot = self.sa2char(begin + pivot_pos, depth);
+        let pivot_pos = (rand::random::<f64>() * size as f64) as i64;
+        let pivot = self.sa2char((begin + pivot_pos) as usize, depth);
 
         while b <= c {
-            let mut b_ch = self.sa2char(b, depth);
+            let mut b_ch = self.sa2char(b as usize, depth);
             while b <= c && b_ch <= pivot {
                 if b_ch == pivot {
-                    self.sa.swap(a, b);
+                    self.sa.swap(a as usize, b as usize);
                     a += 1;
                 }
                 b += 1;
-                if b >= self.size { break; }
-                b_ch = self.sa2char(b, depth);
+                if b >= self.size as i64 { break; }
+                b_ch = self.sa2char(b as usize, depth);
             }
 
-            let mut c_ch = self.sa2char(c, depth);
+            let mut c_ch = self.sa2char(c as usize, depth);
             while b <= c && c_ch >= pivot {
                 if c_ch == pivot {
-                    self.sa.swap(c, d);
+                    self.sa.swap(c as usize, d as usize);
                     d -= 1;
                 }
                 c -= 1;
                 if c < 0 { break; }
-                c_ch = self.sa2char(c, depth);
+                c_ch = self.sa2char(c as usize, depth);
             }
 
             if b > c { break; }
 
-            self.sa.swap(b, c);
+            self.sa.swap(b as usize, c as usize);
             b += 1;
             c -= 1;
         }
 
         let eq_size_a = (a - begin).min(b - a);
         for i in 0..eq_size_a {
-            self.sa.swap(begin + i, b - eq_size_a + i);
+            self.sa.swap((begin + i) as usize, (b - eq_size_a + i) as usize);
         }
 
         let eq_size_d = (d - c).min(end - d);
         for i in 0..eq_size_d {
-            self.sa.swap(b + i, end - eq_size_d + i + 1);
+            self.sa.swap((b + i) as usize, (end - eq_size_d + i + 1) as usize);
         }
 
         self.sort(begin, begin + (b - a) - 1, depth);
@@ -88,14 +88,14 @@ impl BWT {
         self.sa.clear();
         self.size = 0;
 
-        for c in str.chars() {
+        for _ in str.chars() {
             self.sa.push(self.size);
             self.size += 1;
         }
 
-        self.sort(0, self.size - 1, 0);
+        self.sort(0, (self.size - 1) as i64, 0);
 
-        for i in 0..self.size {
+        for i in 0..self.size as usize {
             if self.sa[i] == 0 {
                 self.head = i;
                 break;
